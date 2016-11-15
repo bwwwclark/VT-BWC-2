4a50b04ceb6221ad7a5ce54e3ba654467017fcea
Ben Clark <bwclark@bwclark-macbookpro2.local>
2016-11-15 07:07:03 -0500

    Initial Commit

---
+++ VT-BWC-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VT-BWC-2
+//
+//  Created by Ben Clark on 11/15/16.
+//  Copyright © 2016 Ben Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ VT-BWC-2Tests/VT_BWC_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  VT_BWC_2Tests.swift
+//  VT-BWC-2Tests
+//
+//  Created by Ben Clark on 11/15/16.
+//  Copyright © 2016 Ben Clark. All rights reserved.
+//
+
+import XCTest
+@testable import VT_BWC_2
+
+class VT_BWC_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
